9d46f7a5909a63eac07387a4869e45082a7d985d
tarun.kumar.ghorui <tarun.kumar.ghorui@accenture.com>
2017-12-19 18:04:12 +0530

    test

--- BrowserToWebApp/Controllers/HomeController.cs
+++ BrowserToWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BrowserToWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. ";
 
             return View();
         }
